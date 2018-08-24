c5de42408234cdb21841556903c7df15abc7a0f7
Dat Bac Do <dat.b.do@gmail.com>
2018-08-24 19:30:11 -0400

    add spongebob say

--- src/command.js
+++ src/command.js
@@ -8,4 +8,10 @@ export default {
         }
         getLewlImages(null, query, 1, message.channel);
     },
+    "~say": (message, query) => {
+        const response = query
+            .split("")
+            .map((letter, index) => (index % 2 === 0 ? letter.toLowerCase() : letter.toUpperCase()));
+        message.channel.send(`<@${message.author.id}>: ${response.join("")}`);
+    },
 };
