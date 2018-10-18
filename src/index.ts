b0890adab9a2e057d9fb77ba15840be67e7c0f55
Dat Bac Do <dat.b.do@gmail.com>
2018-10-18 12:17:50 -0400

    fix some line organizing

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,10 @@ import { Client } from "discord.js";
 // https://discordapp.com/oauth2/authorize?client_id=482244091518779402&scope=bot&permissions=8
 
 import * as dotenv from "dotenv";
-dotenv.config();
-
 import { Event } from "./events/event.interface";
 import botEventList from "./events";
-import rule34API from "./lib/api/rule34xxx";
+
+dotenv.config();
 const debug = require("debug")("BotBoi:Main");
 const TOKEN = process.env.BOT_TOKEN;
 
