0ccc4fe5eb745870c4554de9c9c6f531980690f9
Dat Do <dat.b.do@gmail.com>
2018-10-20 10:31:46 -0400

    fix the interval testing

--- src/events/ready/index.ts
+++ src/events/ready/index.ts
@@ -1,5 +1,6 @@
 import { Event } from "../event.interface";
 import { Client } from "discord.js";
+import lolex, { Clock } from "lolex";
 
 import commandList, { COMMANDS } from "../../commands";
 import MyJSONAPI from "../../lib/api/myJson";
@@ -7,6 +8,15 @@ import MyJSONAPI from "../../lib/api/myJson";
 export const RULE34_INTERVAL = 60000 * 15;
 
 const debug = require("debug")("BotBoi:onReadyEvent");
+
+export const setUpMockClock = () => {
+  const clock = lolex.install();
+  return clock;
+};
+
+export const tearDownMockClock = (clock: Clock) => {
+  clock.uninstall();
+};
 
 const ready: Event = {
   eventName: "ready",
@@ -19,7 +29,7 @@ const ready: Event = {
       }
     );
     // recurring
-    client.setInterval(function() {
+    client.setInterval(() => {
       commandList[COMMANDS.RULE34.RULE34_SEARCH](client);
     }, RULE34_INTERVAL);
     debug("Me Me Ready");
--- test/events/ready/index.test.ts
+++ test/events/ready/index.test.ts
@@ -1,11 +1,15 @@
 import { expect } from "chai";
-import readyEvent, { RULE34_INTERVAL } from "../../../src/events/ready";
+import readyEvent, {
+  RULE34_INTERVAL,
+  setUpMockClock,
+  tearDownMockClock
+} from "../../../src/events/ready";
 import { Client, Collection, Guild } from "discord.js";
 import lolex from "lolex";
 
 describe("Ready Event", () => {
   const client: Client = new Client();
-  const clock = lolex.install({ shouldAdvanceTime: true, target: global });
+  const clock = setUpMockClock();
   it("should trigger when ready event got emitted", done => {
     client.on(readyEvent.eventName, () => {
       client.guilds = new Collection<string, Guild>().set(
@@ -13,15 +17,18 @@ describe("Ready Event", () => {
         new Guild(client, { emojis: new Collection() })
       );
       readyEvent.eventActionCallback(client)();
-      clock.tick(RULE34_INTERVAL);
       setTimeout(() => {
         done();
-      }, 1);
+      }, RULE34_INTERVAL + 1);
     });
     client.emit(readyEvent.eventName);
+    clock.tick(RULE34_INTERVAL + 10);
   });
   after(() => {
-    clock.uninstall();
+    setTimeout(() => {
+      tearDownMockClock(clock);
+    }, RULE34_INTERVAL);
+    clock.tick(RULE34_INTERVAL + 10);
     client.destroy();
   });
 });
