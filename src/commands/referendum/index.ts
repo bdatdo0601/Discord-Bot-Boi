e7916468b453b854fca9d40dbca057ffde54bbb4
bluecereal <singedsalam@gmail.com>
2018-10-28 17:24:50 -0400

    fixed a couple mistakes, removed console logging, trim whitespace

--- src/commands/referendum/index.ts
+++ src/commands/referendum/index.ts
@@ -14,16 +14,15 @@ const poll: Command = { // starts vote, must supply options
     query: string,
     message: Message
   ): void => {
-    console.log(votes);
     if (isPoll(votes)) { // vote already in progress
       message.reply("You need to tally before you can start another poll");
     } else {
       let args = query.split(",");  // candidates should be separated by commas
       _.each(args, (candidate: string) => {
+          candidate = _.trim(candidate);
           votes[candidate] = 0;
       });
     }
-    console.log(votes);
   }
 };
 
@@ -35,7 +34,6 @@ const vote: Command = {
     query: string,
     message: Message
   ): void => {
-    console.log(votes);
     if (!isPoll(votes)) {
       message.reply("Bitch you gotta start a poll first!");
     } else if (hasVoted(message.author, voted)) {
@@ -44,8 +42,9 @@ const vote: Command = {
       message.reply(`You can't vote for ${query}, bitch!`);
     } else {
       votes[query] += 1;
+      voted.push(message.author.username + message.author.discriminator);
     }
-    console.log(votes);
+
   }
 };
 
@@ -57,20 +56,19 @@ const tally: Command = {
     query: string,
     message: Message
   ): void => {
-    console.log(votes);
     if (isPoll(votes)) {
       let votes_array: any[] = Object.keys(votes).map(key => ({ key, value: votes[key] })); // maps the votes object properties to objects
-      _.sortBy(votes_array, (obj) => { return obj.value });
-      let winner = votes_array.pop(); // sorted ascending, pop winner from end
+      votes_array = _.sortBy(votes_array, (obj) => { return obj.value });
+      let winner = votes_array.pop(); // sorted ascending, winner at end
       _.each(votes_array, (obj: any) => {
           message.channel.send(`${obj.key} had ${obj.value} votes.`)
       });
       message.channel.send(`The winner is ${winner.key} with ${winner.value} votes!`);
       votes = {};
+      voted = [];
     } else {
       message.reply("Bitch you gotta start a poll first!");
     }
-    console.log(votes);
   }
 };
 
