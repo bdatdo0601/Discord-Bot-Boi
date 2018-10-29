fa545571535f5a5901121a8e9b2c0b7a72ce46b3
Dat Bac Do <dat.b.do@gmail.com>
2018-10-29 13:31:09 -0400

    fix some minor problem

--- src/commands/referendum/index.ts
+++ src/commands/referendum/index.ts
@@ -1,40 +1,47 @@
-import { Client, Message, User } from "discord.js";
+import { Client, Message } from "discord.js";
+import firebase from "firebase";
 import _ from "lodash";
 import { Command } from "../command.interface";
 import { hasVoted, isPoll } from "./helper";
-import {ReferendumCommandKeyList} from "./referendum.interface";
+import {
+  ReferendumCommandKeyList,
+  VoteOptions,
+  VoteResult,
+} from "./referendum.interface";
 // only one poll at a time
-var voted: string[] = [];
-var votes: { [key: string]: number } = {};
-const poll: Command = { // starts vote, must supply options
-  commandDescription: "starts vote, must supply candidates to be voted on",
+const voted: string[] = [];
+const votes: VoteOptions = {};
+const poll: Command = {
+  // starts vote, must supply options
   commandCallback: (
     client: Client,
     db: firebase.database.Database,
     query: string,
-    message: Message
+    message: Message,
   ): void => {
-    if (isPoll(votes)) { // vote already in progress
+    if (isPoll(votes)) {
+      // vote already in progress
       message.reply("You need to tally before you can start another poll");
     } else {
-      let args = query.split(",");  // candidates should be separated by commas
+      const args = query.split(","); // candidates should be separated by commas
       _.each(args, (candidate: string) => {
-          candidate = _.trim(candidate);
-          votes[candidate] = 0;
-          message.channel.send(`${message.author.username} started a poll!`);
-          message.channel.send(`Your choices are ${args}.`);
+        candidate = _.trim(candidate);
+        votes[candidate] = 0;
       });
+      message.channel.send(`${message.author.username} started a poll!`);
+      message.channel.send(`Your choices are [ **${args.join(" ")}** ].`);
     }
-  }
+  },
+  commandDescription:
+    "starts vote, must supply candidates to be voted on separated by comma (`,`)",
 };
 
 const vote: Command = {
-  commandDescription: "votes for a single candidate.  only one vote per poll",
   commandCallback: (
     client: Client,
     db: firebase.database.Database,
     query: string,
-    message: Message
+    message: Message,
   ): void => {
     if (!isPoll(votes)) {
       message.reply("Bitch you gotta start a poll first!");
@@ -47,42 +54,55 @@ const vote: Command = {
       voted.push(message.author.username + message.author.discriminator);
       message.channel.send(`${message.author.username} voted for ${query}!`);
     }
-
-  }
+  },
+  commandDescription: "votes for a single candidate. only one vote per poll",
 };
 
 const tally: Command = {
-  commandDescription: "tallies votes and resets everything",
   commandCallback: (
     client: Client,
     db: firebase.database.Database,
     query: string,
-    message: Message
+    message: Message,
   ): void => {
     if (isPoll(votes)) {
-      let votes_array: any[] = Object.keys(votes).map(key => ({ key, value: votes[key] })); // maps the votes object properties to objects
-      votes_array = _.sortBy(votes_array, (obj) => { return obj.value });
-      let winner = votes_array.pop(); // sorted ascending, winner at end
-      _.each(votes_array, (obj: any) => {
-          message.channel.send(`${obj.key} had ${obj.value} votes.`)
+      // maps the votes object properties to objects
+      const voteResult: VoteResult[] = _.sortBy(
+        Object.keys(votes).map((key) => ({
+          key,
+          value: votes[key],
+        })),
+        (obj) => {
+          return obj.value;
+        },
+      );
+      const winner = voteResult.pop() as VoteResult; // sorted ascending, winner at end
+      _.each(voteResult, (obj: any) => {
+        message.channel.send(`${obj.key} had ${obj.value} votes.`);
       });
-      message.channel.send(`The winner is ${winner.key} with ${winner.value} votes!`);
-      votes = {};
-      voted = [];
+      message.channel.send(
+        `The winner is ${winner.key} with ${winner.value} votes!`,
+      );
+      // clear the object while keeping its immutability
+      for (const voteOption of Object.keys(votes)) {
+        delete votes[voteOption];
+      }
+      voted.length = 0; // clear the array while keeping its immutability
     } else {
       message.reply("Bitch you gotta start a poll first!");
     }
-  }
+  },
+  commandDescription: "tallies votes and resets everything",
 };
 
 export const referendumCommandKeyList: ReferendumCommandKeyList = {
   REFERENDUM_POLL: "~poll",
+  REFERENDUM_TALLY: "~tally",
   REFERENDUM_VOTE: "~vote",
-  REFERENDUM_TALLY: "~tally"
 };
 
 export default {
   [referendumCommandKeyList.REFERENDUM_POLL]: poll,
+  [referendumCommandKeyList.REFERENDUM_TALLY]: tally,
   [referendumCommandKeyList.REFERENDUM_VOTE]: vote,
-  [referendumCommandKeyList.REFERENDUM_TALLY]: tally
 };
