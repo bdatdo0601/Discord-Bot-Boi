2f4b1e39cc4c92aefb014af20c967aa52eebc46a
Dat Bac Do <dat.b.do@gmail.com>
2018-10-29 13:31:09 -0400

    fix some minor problem

--- src/commands/referendum/referendum.interface.ts
+++ src/commands/referendum/referendum.interface.ts
@@ -1,3 +1,12 @@
+export interface VoteOptions {
+  [key: string]: number;
+}
+
+export interface VoteResult {
+  key: string;
+  value: number;
+}
+
 export interface ReferendumCommandKeyList {
   REFERENDUM_POLL: "~poll";
   REFERENDUM_VOTE: "~vote";
